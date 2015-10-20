166978df9bb942cf0bfde1cd42012cc9c3a051c0
Ruel Nopal <radon1284@yahoo.com>
2015-10-20 15:41:42 +0800

    Add minify php snippits for wordpress

---
+++ minify_html.php
@@ -0,0 +1,22 @@
+function minify_html($minify) {
+
+    $search = array(
+        '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',  // strip whitespaces before tags, except space
+        '/(\s)+/s'       // shorten multiple whitespace sequences
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1'
+    );
+
+    $minify = preg_replace($search, $replace, $minify);
+
+    return $minify;
+}
+
+ob_start("minify_html");
+
+add_action('get_header', 'minify_html');
